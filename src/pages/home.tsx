9b5aeda4de849c7db9675e01747f395dd0eacf66
Luiz Torre <luizmptorre@gmail.com>
2021-09-24 11:38:04 -0300

    Alterando as paginas para Tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,14 +3,25 @@ import {View , Text, StyleSheet, TextInput,FlatList} from 'react-native';
 import Button from '../components/button';
 import SkillCard from '../components/SkillCard';
 
+interface SkillData{
+    id: string;
+    name: string;
+
+}
+
+
 export default function Home(){
     const [newSkill, setNewSkill] = useState('');
-    const [mySkills, setMySkills] = useState([])
+    const [mySkills, setMySkills] = useState<SkillData[]>([])
     const [gretting, setGretting] = useState('')
 
 
     function handleAddNewSkill(){
-        setMySkills(oldState => [...oldState, newSkill]);
+        const data ={
+            id : String(new Date().getTime()),
+            name: newSkill
+        }
+        setMySkills(oldState => [...oldState, data]);
     }
 
     useEffect(() => {
@@ -47,9 +58,9 @@ export default function Home(){
 
         <FlatList 
             data = {mySkills}
-            keyExtractor = {item => item}
+            keyExtractor = {item => item.id}
             renderItem = {({item}) => ( 
-                <SkillCard skill = {item}/>) }
+                <SkillCard skill = {item.name}/>) }
         />
 
 
